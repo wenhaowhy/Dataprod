4b7fd0aece1ea117f38163f0e602ddb09be3efe2
wenhaowhyy <wyu2013@gmail.com>
2016-05-10 17:16:14 -0400

    added 2 files

---
+++ bac/server.R
@@ -0,0 +1,18 @@
+alc_content <- function(drinks) as.numeric(drinks) *14
+weight <- function(bmi) as.numeric(bmi) * 453.592
+bac <- function(Drinks, weight, time){
+  (((as.numeric(Drinks)*14)/(as.numeric(weight)* 453.592 * 0.68))*100 - (as.numeric(time)*0.015))}
+
+
+
+shinyServer(
+  function(input, output) {
+    output$alcohol <- renderPrint({alc_content(input$drinks)})
+    output$weight1 <- renderPrint({weight(input$weight)})
+    output$time1 <- renderPrint({input$time})
+    output$BAC <- renderPrint({bac(input$drinks, input$weight,
+                                   input$time)})
+    
+    
+  }
+)
